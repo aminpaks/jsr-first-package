1d9421776015936efa189fc6e638c7a0ee3d7fe8
Amin Pakseresht <amin.pakseresht@hotmail.com>
2024-02-26 22:15:38 -0500

    Add version for ldoash.isplainobject

--- hello.ts
+++ hello.ts
@@ -1,11 +1,11 @@
 import {readFileSync} from 'node:fs';
-import isPlainObject from 'npm:lodash.isplainobject';
+import isPlainObject from 'npm:lodash.isplainobject@4.0.6';
 
-export function init(filepath: string) {
+export function init(filepath: string): void {
   const data = readFileSync(filepath, 'utf8');
   console.log({data, isData: isPlainObject(data)});
 }
 
-export function version() {
+export function version(): string {
   return '0.0.1';
 }
